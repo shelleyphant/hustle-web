92de466a13a5fc21adb847ef84b2e14932207be0
Shelley Bassett <m.bassett@y7mail.com>
2021-10-07 12:25:36 +1100

    cleaning up connection settings

--- server-index.js
+++ server-index.js
@@ -4,11 +4,6 @@ if (process.env.NODE_ENV !== 'production') {
 
 const express = require('express')
 const app = express()
-// const passport = require('passport')
-// const flash = require('express-flash')
-// const session = require('express-session')
-// const methodOverride = require('method-override')
-// const cors = require('cors')
 
 const port = process.env.PORT || 5000
 const db = require('./connection')
@@ -25,6 +20,14 @@ db.connection((error) => {
 })
 
 app.use('/auth', require('./routes/auth'))
+
+
+
+// const passport = require('passport')
+// const flash = require('express-flash')
+// const session = require('express-session')
+// const methodOverride = require('method-override')
+// const cors = require('cors')
 
 // app.use(cors())
 // app.use(express.urlencoded({ extended: false }))
